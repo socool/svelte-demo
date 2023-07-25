9cef4b865992fb48cdc2fdbd5588cab63c634c0e
So Cool <127.0.0.1.pm@gmail.com>
2023-07-25 22:50:42 +0700

    simple component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script>
+    import Nested from "./nested.svelte";
     let name = "svelte";
     let gif = "public/test.gif";
 </script>
@@ -12,4 +13,5 @@
 </style>
 <h1>Welcome to SvelteKit</h1>
 <p>{name.toUpperCase()}</p>
+<Nested/>
 <img src={gif} alt="simpson dance">
