c91c07a20cf2c4e1d343d7a75cce93ea063ebdac
So Cool <127.0.0.1.pm@gmail.com>
2023-07-25 22:54:20 +0700

    add HTML tag

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,7 @@
     import Nested from "./nested.svelte";
     let name = "svelte";
     let gif = "public/test.gif";
+    let string = `this string contains some <strong>HTML!!!</strong>`;
 </script>
 <style>
 	/* Write your CSS here */
@@ -13,5 +14,11 @@
 </style>
 <h1>Welcome to SvelteKit</h1>
 <p>{name.toUpperCase()}</p>
-<Nested/>
 <img src={gif} alt="simpson dance">
+
+<h2>Component</h2>
+<Nested/>
+
+<h2>Html Tag</h2>
+<p>{string}</p>
+<p>{@html string}</p>
