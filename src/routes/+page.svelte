c8e7dedb982f94d25b18fd8e5baceb7b072f92e5
So Cool <127.0.0.1.pm@gmail.com>
2023-07-25 22:43:41 +0700

    add style

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,14 @@
     let name = "svelte";
     let gif = "public/test.gif";
 </script>
+<style>
+	/* Write your CSS here */
+	p {
+		color: goldenrod;
+		font-family: 'Comic Sans MS', cursive;
+		font-size: 2em;
+	}
+</style>
 <h1>Welcome to SvelteKit</h1>
 <p>{name.toUpperCase()}</p>
 <img src={gif} alt="simpson dance">
