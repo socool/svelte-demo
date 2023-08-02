c23ecb8b6604cadcd7a993bfb78e84b8daa10a6d
So Cool <127.0.0.1.pm@gmail.com>
2023-08-02 17:14:01 +0700

    DOM events

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -51,6 +51,7 @@
     <a href="/part1/logic">Logic</a>
     <a href="/part1/logic/keyed">Keyed each blocks</a>
     <a href="/part1/logic/await">Await blocks</a>
+    <a href="/part1/logic/event">Event</a>
 </nav>
 
 <h2>Component</h2>
